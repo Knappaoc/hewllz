1640c629621760f5c0db20dffb5832b94e02f0fa
Zoey Llewellyn Hewll <zoeyhewll@gmail.com>
2022-12-05 23:11:20 +0800

    mid part 5

--- src/day05.rs
+++ src/day05.rs
@@ -1,4 +1,7 @@
-use std::io::Read;
+use chumsky::{error::Cheap, prelude::*, text::int};
+use std::{io::Read, str::FromStr};
+
+use anyhow::Result;
 
 pub fn main(input: impl Read) -> Result<String, !> {
     main_01(input)
@@ -8,28 +11,134 @@ fn main_01(_input: impl Read) -> Result<String, !> {
     todo!()
 }
 
-fn _main_02(_input: impl Read) -> Result<String, !> {
+fn main_02(_input: impl Read) -> Result<String, !> {
     todo!()
 }
 
-/*
+#[derive(PartialEq, Eq, PartialOrd, Ord, Debug)]
+struct Instruction {
+    count: usize,
+    from: usize,
+    to: usize,
+}
+impl FromStr for Instruction {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        todo!()
+    }
+}
+
+#[derive(PartialEq, Eq, PartialOrd, Ord, Debug)]
+struct Stacks(Vec<Vec<char>>);
+impl FromStr for Stacks {
+    type Err = Vec<Simple<char>>;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        // A parser that parses any number of whitespace characters without allocating
+        Self::parser().parse(s)
+    }
+}
+impl Stacks {
+    fn parser() -> impl Parser<char, Self, Error = Simple<char>> {
+        todo!()
+    }
+}
+impl Instruction {
+    fn parser() -> impl Parser<char, Self, Error = Simple<char>> {
+        let whitespace = filter::<_, _, Cheap<char>>(|c: &char| c.is_whitespace())
+            .ignored()
+            .repeated();
+        let mov = text::keyword("move").ignored().padded();
+        let from = just("from").padded();
+        let to = just("to").ignored().padded();
+        let int = text::int(10).map(|s: String| s.parse()).padded();
+        /*
+        let parser = mov
+            .then(int)
+            .map(|s: &str| s.parse())
+            .padded()
+            .then(from)
+            .then(int)
+            .padded()
+            .then(to)
+            .padded()
+            .then(int)
+            .padded()
+            .map(|x| Instruction {
+                from: 0,
+                to: 0,
+                count: 0,
+            });
+        parser
+        */
+        todo!()
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use indoc::indoc;
 
+    use crate::day05::{Instruction, Stacks};
+
     use super::{main_01, main_02};
 
     #[test]
+    fn test_parse_instructions() {
+        let file = "move 1 from 2 to 3";
+
+        let expected = Instruction {
+            count: 1,
+            from: 2,
+            to: 3,
+        };
+        assert_eq!(expected, file.parse().unwrap());
+    }
+
+    #[test]
+    fn test_parse_stacks() {
+        let file = indoc!(
+            "    [D]    
+            [N] [C]    
+            [Z] [M] [P]
+             1   2   3 "
+        );
+
+        let expected = Stacks(vec![vec!['Z', 'N'], vec!['M', 'C', 'D'], vec!['P']]);
+        assert_eq!(expected, file.parse().unwrap());
+    }
+
+    #[test]
     fn test_main_01() {
-        let file = indoc!("");
+        let file = indoc!(
+            "    [D]    
+            [N] [C]    
+            [Z] [M] [P]
+             1   2   3 
+
+            move 1 from 2 to 1
+            move 3 from 1 to 3
+            move 2 from 2 to 1
+            move 1 from 1 to 2"
+        );
         let res = main_01(file.as_bytes()).unwrap();
-        assert_eq!(&*res, "");
+        assert_eq!(&*res, "CMZ");
     }
     #[test]
     fn test_main_02() {
-        let file = indoc!("");
+        let file = indoc!(
+            "    [D]    
+            [N] [C]    
+            [Z] [M] [P]
+             1   2   3 
+
+            move 1 from 2 to 1
+            move 3 from 1 to 3
+            move 2 from 2 to 1
+            move 1 from 1 to 2"
+        );
         let res = main_02(file.as_bytes()).unwrap();
         assert_eq!(&*res, "");
     }
 }
-*/
